8b315e52095ac60ebbac2a25ec51f24581a1cb70
Rohan Mehta <117997373+rohanisstuckinvim@users.noreply.github.com>
2022-11-16 12:07:44 -0500

    Create Crystal Explosion.py

---
+++ Unit 1/1.3.6 Exercises/Crystal Explosion.py
@@ -0,0 +1,10 @@
+# Draws the background.
+Rect(0, 0, 400, 400)
+
+# Draw a bunch of stars to create the crystal.
+### (HINT: The Inspector results are ordered in a particular fashion!)
+### Place Your Code Here ###
+Star(200, 200, 200, 13, fill =None, roundness = 10, dashes = True, border = 'aliceBlue')
+Star(200, 200, 150, 11, fill =None, roundness = 20, dashes = True, border = 'aliceBlue')
+Star(200, 200, 100, 7, fill =None, roundness = 5, dashes = True, border = 'aliceBlue')
+Star(200, 200, 50, 19, fill =None, roundness = 50, dashes = True, border = 'aliceBlue')
