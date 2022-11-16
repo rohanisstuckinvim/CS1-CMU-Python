07ba77bf90fe6a52ea13b1c6b713a650bc9ca1de
Rohan Mehta <117997373+rohanisstuckinvim@users.noreply.github.com>
2022-11-16 11:57:56 -0500

    Create 3D Glasses.py

---
+++ Unit 1/1.2.6 Exercises/3D Glasses.py
@@ -0,0 +1,19 @@
+# Draws the background color.
+Rect(0, 0, 400, 400, fill='tan')
+
+# Draws the eyes and smile.
+Rect(125, 80, 30, 80)
+Rect(245, 80, 30, 80)
+Rect(50, 200, 25, 75)
+Rect(325, 200, 25, 75)
+Rect(75, 275, 250, 40)
+
+# Draws the glasses frame.
+Rect(0, 100, 60, 40, fill='white')
+Rect(185, 110, 30, 20, fill='white')
+Rect(340, 100, 60, 40, fill='white')
+
+# Draw the glasses.
+### Place Your Code Here ###
+Rect(60, 60, 125, 120, fill = 'red', opacity = 60)
+Rect(215, 60, 125, 120, fill = 'blue', opacity = 60)
