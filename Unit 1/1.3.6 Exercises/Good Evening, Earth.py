00bdcbdf2ca4455b72d1fc0a79d5e6a26448192e
Rohan Mehta <117997373+rohanisstuckinvim@users.noreply.github.com>
2022-11-16 12:07:01 -0500

    Create Good Evening, Earth.py

---
+++ Unit 1/1.3.6 Exercises/Good Evening, Earth.py
@@ -0,0 +1,13 @@
+# Draws the computer and screen.
+Rect(0, 0, 400, 400, border='dimGrey', borderWidth=40)
+
+# Draw the text on the computer's screen.
+### Place Your Code Here ###
+
+Label('System Loading...', 140, 75, fill = 'lime', font = 'arial', size = 18)
+Label('Accepting Inputs:', 135, 100, fill = 'lime', font = 'arial', size = 18)
+
+Label('> python hello_world.py', 195, 140, fill = 'lime', font = 'montserrat', size = 20)
+
+Label('Good Evening, Earth', 170, 200, fill = 'lime', font = 'orbitron', size = 18)
+
