b1b041ba195f7c2f7b263363ad7303e574f34adf
Rohan Mehta <117997373+rohanisstuckinvim@users.noreply.github.com>
2022-11-16 12:09:10 -0500

    Create Tropical Island.py

---
+++ Unit 1/1.4 Unit 1 Exercises/Tropical Island.py
@@ -0,0 +1,19 @@
+# Draws the sky.
+Rect(0, 0, 400, 200, fill='skyBlue')
+
+# Draw the tropical island!
+### Place Your Code Here ###
+
+Oval(200, 200, 300, 80, fill = 'wheat')
+Oval(200, 170, 200, 20, fill = 'green')
+Polygon(255, 75, 250, 150, 245, 170, 235, 165, 240, 155, fill = 'sienna')
+Star(255, 75, 50, 9, fill = 'limeGreen', roundness = 50)
+Circle(230, 85, 6, fill = 'maroon')
+Star(255, 75, 40, 8, fill = 'Green', roundness = 50)
+
+Circle(245, 80, 6, fill = 'maroon')
+
+Circle(250, 100, 6, fill = 'maroon')
+
+# Draws the ocean.
+Rect(0, 200, 400, 200, fill='navy')
