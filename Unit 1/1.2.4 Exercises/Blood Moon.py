071c7f3b85e56c100a3d55182ef182ac1865d12c
Rohan Mehta <117997373+rohanisstuckinvim@users.noreply.github.com>
2022-11-16 11:56:40 -0500

    Create Blood Moon.py

---
+++ Unit 1/1.2.4 Exercises/Blood Moon.py
@@ -0,0 +1,5 @@
+# Use gradients to draw the two rectangles.
+### (HINT: The gradients use both rgb and named colors!)
+### Place Your Code Here ###
+Rect(0, 0, 400, 300, fill=gradient('orangeRed', rgb(60, 20, 20,), 'black'))
+Rect(0, 300, 400, 100, fill=gradient(rgb(150, 50 , 0), rgb(20, 20, 100)))
