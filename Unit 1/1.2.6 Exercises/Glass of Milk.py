20bcda5244b1f1fbf03368ca9efd2067109aa72d
Rohan Mehta <117997373+rohanisstuckinvim@users.noreply.github.com>
2022-11-16 11:58:17 -0500

    Create Glass of Milk.py

---
+++ Unit 1/1.2.6 Exercises/Glass of Milk.py
@@ -0,0 +1,8 @@
+# Draws the background and table.
+Rect(0, 0, 400, 320, fill='steelBlue')
+Rect(0, 320, 400, 80, fill='sienna')
+
+# Draw the milk and the glass.
+### Place Your Code Here ###
+Rect(110, 120, 180, 200, fill = 'white')
+Rect(110, 80, 180, 240, fill = gradient('lightGrey', 'grey', 'lightGrey', start='left'), opacity = 50)
