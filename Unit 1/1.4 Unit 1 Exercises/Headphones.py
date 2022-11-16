c403de38ed14aec65d30b9a03f7b0e19b1d232d4
Rohan Mehta <117997373+rohanisstuckinvim@users.noreply.github.com>
2022-11-16 12:10:15 -0500

    Create Headphones.py

---
+++ Unit 1/1.4 Unit 1 Exercises/Headphones.py
@@ -0,0 +1,13 @@
+# Draw the headphones.
+### (HINT: You will need four shapes. One of them is a Rect with a fill of
+#          white that covers part of a circle.)
+### Place Your Code Here ###
+Circle(200, 190, 150, fill =None, border = 'black', borderWidth = 10)
+Rect(0, 160, 400, 200,  fill ='White')
+Circle(200, 200, 125, fill =None, border = 'black', borderWidth = 20)
+Rect(40, 160, 40, 80, fill = 'black')
+Rect(320, 160, 40, 80, fill = 'black')
+# Draw the head and body.
+### Place Your Code Here ###
+
+Line(200, 325, 200,400, fill = 'black', lineWidth = 20)
