5a674f5847fcfdcaad6372901f44d15665ad4c25
Rohan Mehta <117997373+rohanisstuckinvim@users.noreply.github.com>
2022-11-16 11:53:49 -0500

    Create Diamond Pickaxe.py

---
+++ Unit 1/1.2.4 Exercises/Diamond Pickaxe.py
@@ -0,0 +1,11 @@
+# Color the handle wood.
+### Modify Your Code Here ###
+Rect(50, 275, 75, 75, fill=gradient('peru', 'maroon', start='center'))
+Rect(125, 200, 75, 75, fill=gradient('peru', 'maroon', start='center'))
+Rect(200, 125, 75, 75, fill=gradient('peru', 'maroon', start='center'))
+Rect(300, 50, 50, 50, fill=gradient('peru', 'maroon', start='center'))
+
+# Give the pickaxe a diamond gradient!
+## Modify Your Code Here ###
+Rect(75, 50, 200, 50, fill=gradient('royalBlue', 'deepSkyBlue', 'dodgerBlue', start='top'))
+Rect(300, 125, 50, 200, fill=gradient('royalBlue', 'deepSkyBlue', 'dodgerBlue', start='right'))
