568288563de5271be5e5daa5f6b484266a24ab42
Rohan Mehta <117997373+rohanisstuckinvim@users.noreply.github.com>
2022-11-16 12:07:25 -0500

    Create Cube.py

---
+++ Unit 1/1.3.6 Exercises/Cube.py
@@ -0,0 +1,9 @@
+# First, draw the shadow of the cube.
+### Place Your Code Here ###
+Polygon(95, 270, 325, 205, 400, 205, 400, 285, 235, 350, fill = 'black', opacity = 30)
+# Then draw the cube itself.
+### Place Your Code Here ###
+Polygon(95, 70, 295, 70, 235, 150, 35, 150, fill = 'darkGrey')
+Polygon(235, 150, 295, 70, 295, 270, 235, 350, fill = 'grey')
+
+Rect(35, 150, 200, 200, fill = 'silver')
