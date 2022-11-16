251a1f3f8b959a7ffbd1bca027be69fa72fab930
Rohan Mehta <117997373+rohanisstuckinvim@users.noreply.github.com>
2022-11-16 12:03:53 -0500

    Create Target Practice.py

---
+++ Unit 1/1.3.2 Exercises/Target Practice.py
@@ -0,0 +1,16 @@
+# Draws the background.
+Rect(0, 0, 400, 400, fill='ghostWhite')
+
+# Draw the rope that the target hangs from.
+### (HINT: This exercise requires some careful use of the Inspector!)
+### Place Your Code Here ###
+
+# Now draw the target.
+### Place Your Code Here ###
+
+Line(0, 0 ,200, 225, fill = 'dimGrey', lineWidth = 6, dashes = True)
+Line(400, 0 ,200, 225, fill = 'dimGrey', lineWidth = 6, dashes = True)
+
+Circle(200, 225, 150, fill = 'dodgerBlue', border = 'black', borderWidth = 2)
+Circle(200, 225, 100, fill = 'crimson', border = 'black', borderWidth = 1)
+Circle(200, 225, 45, fill = 'yellow', border = 'black', borderWidth = 1)
